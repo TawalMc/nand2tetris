3e494eb446439192630499503ccbc9e1b91092c9
Tawaliou ALAO <tawaliou.alao@erugis.fr>
2024-04-12 22:12:49 +0100

    return argument 1&2 of instructions

--- projects/07/VMTranslator/main.py
+++ projects/07/VMTranslator/main.py
@@ -1,4 +1,4 @@
-from parser.constants import C_ARITHMETIC, C_RETURN
+from parser.constants import C_ARITHMETIC, C_RETURN, C_PUSH, C_POP, C_FUNCTION, C_CALL
 from parser.parser import Parser
 
 if __name__ == '__main__':
@@ -8,7 +8,10 @@ if __name__ == '__main__':
         command = parser.command_type()
         if not command:
             continue
+        print(parser.current_command)
         if command != C_RETURN:
-            print("ar:", parser.arg1())
+            print("ar1:", parser.arg1())
+        if command in [C_PUSH, C_POP, C_FUNCTION, C_CALL]:
+            print("ar2:", parser.arg2())
 
     pass
